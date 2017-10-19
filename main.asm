9e7174319eb7dde9b4d9f138204ef6f62cdf6470
sebastian López Fernández <sebastian Lpez Fernndez>
2017-10-19 14:44:00 -0500

    led enciende y apaga

--- main.asm
+++ main.asm
@@ -15,22 +15,45 @@
     BCF STATUS, RP0	;volver al banco 0
     BCF STATUS, RP1
     
+    
+    
        
-    inicio_retardo	;subrrutina retardo
-    movlw d'250'	;movemos el valor 250
-    movwf CONTA_1	;movemos el valor a CONTA_1
-    movlw d'250'	;movemos el valor 250
-    movwf CONTA_2	;movemos el valor a CONTA_2
-    movlw d'4'		;movemos el valor 4
-    movwf CONTA_3	;movemos el valor a CONTA_3
-    nop			;retardo de 1 uS
-    decfsz CONTA_1, f	;decrementa CONTA_1 en 1,(CONTA_1 - 1) si es 0 salta
-    goto $-.2		;se devuleve 2 lineas
-    decfsz CONTA_2, F	;decrementa CONTA_2 en 1,(CONTA_2 - 1) si es 0 salta
-    goto $-.4		;Se devuelve 6 lineas
-    decfsz CONTA_3, F	;decrementa CONTA_3 en 1,(CONTA_3 - 1) si es 0 salta
-    goto $-.6		;Se devuelve 8 lineas
-    movlw b'00000000'	;apagamos el led
-    movwf TRISB		;ubicado en el puerto B
-    goto inicio_retardo    ;regresa a la subrrutina 
+    retardo	;subrrutina retardo
+	;movlw b'00000001'
+	;movwf TRISB
+	movlw d'4'	;movemos el valor 4
+	movwf CONTA_3	;movemos el valor a CONTA_1
+	movlw d'250'	;movemos el valor 250
+	movwf CONTA_2	;movemos el valor a CONTA_2
+	movlw d'250'		;movemos el valor 250
+	movwf CONTA_1	;movemos el valor a CONTA_3
+	nop			;retardo de 1 uS
+	decfsz CONTA_1, f	;decrementa CONTA_1 en 1,(CONTA_1 - 1) si es 0 salta
+	goto $-.2		;se devuleve 2 lineas
+	decfsz CONTA_2, F	;decrementa CONTA_2 en 1,(CONTA_2 - 1) si es 0 salta
+	goto $-.6		;Se devuelve 6 lineas
+	decfsz CONTA_3, F	;decrementa CONTA_3 en 1,(CONTA_3 - 1) si es 0 salta
+	goto $-.8		;Se devuelve 8 lineas
+	movlw b'00000000'	;apagamos el led
+	movwf TRISB		;ubicado en el puerto B
+	goto retardo2	;va a la subrrutina 2
+    
+    retardo2
+	movlw d'4'	;movemos el valor 4
+	movwf CONTA_3	;movemos el valor a CONTA_1
+	movlw d'250'	;movemos el valor 250
+	movwf CONTA_2	;movemos el valor a CONTA_2
+	movlw d'250'		;movemos el valor 250
+	movwf CONTA_1	;movemos el valor a CONTA_3
+	nop			;retardo de 1 uS
+	decfsz CONTA_1, f	;decrementa CONTA_1 en 1,(CONTA_1 - 1) si es 0 salta
+	goto $-.2		;se devuleve 2 lineas
+	decfsz CONTA_2, F	;decrementa CONTA_2 en 1,(CONTA_2 - 1) si es 0 salta
+	goto $-.6		;Se devuelve 6 lineas
+	decfsz CONTA_3, F	;decrementa CONTA_3 en 1,(CONTA_3 - 1) si es 0 salta
+	goto $-.8		;Se devuelve 8 lineas
+	movlw b'11111111'	;apagamos el led
+	movwf TRISB		;ubicado en el puerto B
+	goto retardo	;va a la subrrutina 2
+	
 end			;finaliza el programa 
